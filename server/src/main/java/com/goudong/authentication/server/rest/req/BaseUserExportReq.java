210e867a47f48bbd263b915b0c543cb1849ef81d
cfl <1696741038@qq.com>
2024-01-12 13:02:10 +0800

    init

---
+++ server/src/main/java/com/goudong/authentication/server/rest/req/BaseUserExportReq.java
@@ -0,0 +1,25 @@
+package com.goudong.authentication.server.rest.req;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import java.util.List;
+
+/**
+ * 类描述：
+ * 导出用户
+ * @author chenf
+ * @version 1.0
+ */
+@Data
+public class BaseUserExportReq {
+    //~fields
+    //==================================================================================================================
+    @ApiModelProperty(value = "勾选的id")
+    private List<Long> ids;
+
+    @ApiModelProperty(value = "分页查询参数")
+    private BaseUserPageReq pageReq;
+    //~methods
+    //==================================================================================================================
+}
---
+++ src/main/java/com/goudong/Main.java
@@ -0,0 +1,7 @@
+package com.goudong;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
