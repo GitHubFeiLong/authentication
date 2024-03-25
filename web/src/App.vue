f0e95b80e24593a8474e8b51754bc078d1edd460
cfl <1696741038@qq.com>
2024-03-25 18:13:11 +0800

    锁屏的逻辑未完成

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,10 +1,12 @@
 <template>
   <div id="app">
     <router-view />
+    <Lock v-if="isLock == 'true'" />
   </div>
 </template>
 
 <script>
+import lockMixin from "@/views/mixins/lock.vue"
 export default {
   name: 'App'
 }
